8a888c1bde5a16c36536e531d82561a7545c9682
GuerinTifenn <8807@holbertonstudents.com>
2024-04-23 10:14:27 +0200

    remplace strdup, update getenv

--- environment.c
+++ environment.c
@@ -11,11 +11,14 @@ char *_getenv(const char *name)
 	char **env = environ;
 	int name_len = strlen(name);
 
+	if (name == NULL || environ == NULL)
+			return (NULL);
+			
 	while (*env != NULL)
 	{
 		if (strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
 		{
-			return (&(*env)[name_len + 1]); /* Return the value after '=' */
+			return (*env + name_len + 1); /* Return the value after '=' */
 		}
 		env++;
 	}
@@ -34,4 +37,36 @@ void print_environment(void)
 		printf("%s\n", *env_var);
 		env_var++;
 	}
+}
+
+/**
+ * _strdup - copy a string and returns pointer to a allocated space in memory
+ * @str: the string to duplicate
+ *
+ * Return: NULL if str = 0 or if insufficient memory
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+	char *dest;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	len++;
+
+	dest = malloc(len + 1);
+
+	if (dest == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+	dest[i] = str[i];
+	i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
